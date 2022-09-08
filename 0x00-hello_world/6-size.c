18bf542ce2eeada38ec3564707dac8e96b3cd296
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-09-08 08:13:43 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-	puts("Size of a char: %d byte(s)", sizeof(char));
-	puts("Size of a int: %d byte(s)", sizeof(int));
-	puts("Size of a long int: %d byte(s)", sizeof(long int));
-	puts("Size of a long long int: %d byte(s)", sizeof(long long int));
-	puts("Size of a float: %d byte(s)", sizeof(float));
+	pirintf("Size of a char: %d byte(s)", sizeof(char));
+	printf("Size of a int: %d byte(s)", sizeof(int));
+	printf("Size of a long int: %d byte(s)", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)", sizeof(long long int));
+	printf("Size of a float: %d byte(s)", sizeof(float));
 	return (0);
 }
