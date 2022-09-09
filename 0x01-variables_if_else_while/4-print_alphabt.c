0ddfc53fb60f7acee961428e44922e641165c5f6
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-09-09 07:58:17 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+		if (n == 'q' || n == 'e')
+			continue;
+		putchar(n);
+	return (0);
+}
