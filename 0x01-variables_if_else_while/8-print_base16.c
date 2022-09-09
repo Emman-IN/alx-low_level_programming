4549352dfd63aeae74d3a58f065c8dc923584d6a
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-09-09 12:06:49 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (SUccess)
+*/
+
+int main(void)
+{
+
+	int i, j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	for (j = 97; j <= 102; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
