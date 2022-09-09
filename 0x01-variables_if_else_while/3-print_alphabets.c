d778bfdd2c6e333216219ba110dfb99cee63b20a
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-09-09 07:33:41 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n, m;
+
+	for (n = 97, m = 65; n <= 122, m = 90; m++, n++)
+		putchar(n)
+		putchar(m)
+	putchar('\n')
+	return (0)
+}
