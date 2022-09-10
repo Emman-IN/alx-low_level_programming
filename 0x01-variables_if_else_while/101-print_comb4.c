4e85c83678a21b28f053bd1af4e4684c75cef226
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-09-10 02:51:28 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+/**
+* main - start
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 48 ; b <= 57; b++)
+		{
+			for (c = 48 ; c <= 57; c++)
+			{
+				if (a != b && b != c && a != c)
+				{
+					if (a < b && b < c && a < c)
+					{	putchar(a);
+						putchar(b);
+						putchar(c);
+						if (a == 55 && b == 56 && c == 57)
+						{
+							break;
+						}
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
