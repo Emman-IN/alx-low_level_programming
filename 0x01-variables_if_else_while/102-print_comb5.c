d391fde0af52a1f5f9549fa77538f1fca5f8adea
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-09-12 08:36:32 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success0
+*/
+
+int main(void)
+{
+	for (int outer = 0; outer <= 99; outer++)
+	{
+		for (int inner = 1; inner <= 99; inner++)
+		{
+			if (outer != inner && outer < inner)
+			{
+				printf("%02d", outer);
+				printf(" ");
+				printf("%02d", inner);
+				if (!(outer == 98 && inner == 99))
+				{
+					printf(",");
+					printf(" ");
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
