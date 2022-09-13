27aa492fa1d6f0a8208a62f165330afe8ad9562b
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-09-13 04:30:30 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lowercase alphabet ten times
+ */
+
+void print_alphabet_x10(void)
+{
+	int times, i;
+
+	for (times = 0; times <= 10; times++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+
+	}
+}
+
