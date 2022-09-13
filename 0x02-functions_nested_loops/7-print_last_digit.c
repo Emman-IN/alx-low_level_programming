6ed0f61b82459f7501cca3f14ac7563e3a480a82
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-09-13 08:41:52 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: number to be evaluated
+ * Return: return 0 success
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+	return (last);
+}
+
