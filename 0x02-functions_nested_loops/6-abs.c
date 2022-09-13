7332d93fb2ed577bf4394176b597b0dc8bdaa202
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-09-13 08:17:34 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ * @int: parameter to be evaluated
+ * Return: 0 Success always
+ */
+
+int _abs(int);
+{
+	n;
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
