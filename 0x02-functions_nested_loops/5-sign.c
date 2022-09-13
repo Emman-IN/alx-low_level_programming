6771e0b9262bb74dacb4699d23f0c8ec940beea0
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-09-13 06:25:51 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,22 +7,23 @@
  * Return: 1 if positive , 0 if zero and -1 if negative
  */
 
+
 int print_sign(int n)
 {
-	if (n == 0)
-	{
-		_putchar(48);
-		return (0);
-	}
-	else if (n > 0)
+	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else 
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
 	}
 	_putchar('\n');
 }
