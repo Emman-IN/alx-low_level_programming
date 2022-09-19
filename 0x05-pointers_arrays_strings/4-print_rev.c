9b9097304492505979a9af12b1122e3761282600
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-09-19 05:54:17 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,22 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in reverse
+ * _strlen - gets the length of the string
+ * print_rev - reverse the string
  * @s: string to be reversed
+ * Return: the integer length of the string
  */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+
+	}
+	return (i);
+}
 
 void print_rev(char *s)
 {
