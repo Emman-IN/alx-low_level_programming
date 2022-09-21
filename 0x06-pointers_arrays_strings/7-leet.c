4360011cb1a2d0aabdbbf2241576a0622d8ab6b6
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-09-21 15:21:03 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,29 +8,18 @@
 
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char word[] = "aAeEoOtTlL";
+	char encoding[] = "4433007711";
 
 	for (i = 0; s[i]; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
+		for (j = 0; j < 10; j++)
 		{
-			s[i] = '4';
-		}
-		else if (s[i] == 'e' || s[i] == 'E')
-		{
-			s[i] = '3';
-		}
-		else if (s[i] == 'o' || s[i] == 'O')
-		{
-			s[i] = '0';
-		}
-		else if (s[i] == 't' || s[i] == 'T')
-		{
-			s[i] = '7';
-		}
-		else if (s[i] == 'L' || s[i] == 'l')
-		{
-			s[i] = '1';
+			if (s[i] == word[j])
+			{
+				s[i] = encoding[j];
+			}
 		}
 	}
 	return (s);
