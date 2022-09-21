21042ec627b224fc0fb71833835a18012a3ddf1a
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-09-21 15:39:15 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: number to print
+ */
+
+void print_number(int n)
+{
+	if (n >= 0)
+	{
+		if (n == 0)
+		{
+			_putchar(n + '0');
+		}
+		else if (n > 0 && n <= 9)
+		{
+			_putchar(n % 10 + '0');
+		}
+
+		else if (n > 9)
+		{
+			print_number(n / 10);
+			print_number(n % 10);
+		}
+	}
+	else if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+		if (n > 0 && n <= 9)
+		{
+			_putchar(n % 10 + '0');
+		}
+		else if (n > 9)
+		{
+			print_number(n / 10);
+			print_number(n % 10);
+		}
+	}
+}
