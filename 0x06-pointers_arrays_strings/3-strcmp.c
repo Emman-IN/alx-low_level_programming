93bd31e8e5b0617302427e3031f4d9c0909be117
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-09-21 14:49:17 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,13 +13,10 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] && s2[i]; i++)
 	{
-		if (s1[i] == s2[i])
-		{
-			return (0);
-		}
-		else
+		if (s1[i] != s2[i])
 		{
 			return (s1[i] - s2[i]);
 		}
 	}
+	return (0);
 }
