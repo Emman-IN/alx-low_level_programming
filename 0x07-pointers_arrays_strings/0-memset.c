ffe8381d254a3b253c788c069c6736a61b7b93b2
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-09-26 12:54:51 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,7 +10,7 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
@@ -18,4 +18,5 @@ char *_memset(char *s, char b, unsigned int n)
 	}
 	s[i] = '\0';
 	return (s);
+
 }
