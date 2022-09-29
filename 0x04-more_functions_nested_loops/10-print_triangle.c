d80f30aeb190ecf637fb0fd71e9a557d303532c1
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-09-29 02:29:16 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,7 +15,7 @@ void print_triangle(int size)
 	{
 		for (i = 1; i <= size; i++)
 		{
-			for (j =1; j <= size; j++)
+			for (j = 1; j <= size; j++)
 			{
 				if (i + j <= size)
 				{
@@ -29,5 +29,6 @@ void print_triangle(int size)
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
