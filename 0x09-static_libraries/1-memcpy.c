96efe9e4925217c1a6289631efcacddc1a6b179d
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-09-30 10:14:33 -0700

    0

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _memset - function that fills memory with a constant byte
- * @s: memory to be filled
- * @b: character to be filled in memory
- * @n: number of character in the memory
- * Return: Char pointer to memory
+ * _memcpy - function that copies memory area
+ * @dest: destination
+ * @src: source
+ * @n: number of items from destination
+ * Return: char pointer
  */
 
-char *_memset(char *s, char b, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		s[i] = b;
+		dest[i] = src[i];
 	}
 
-	return (s);
+	return (dest);
 }
