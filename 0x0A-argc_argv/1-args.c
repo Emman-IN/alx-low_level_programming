4416616b8f5990313be884ae8cbbea7e30107ce3
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-09-30 11:32:57 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by newline
+ * @argc: argument count
+ * @argv: arguement vector (1-D array of strings)
+ * Return: Always 0 (success)
+ */
+
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
