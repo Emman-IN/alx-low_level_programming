22bd047d05d9545d83a0b163769ecbc54abf6f6a
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-09-30 12:03:48 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: arguement count
+ * @argv: arguement vector (array of arguement strings)
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	int m;
+
+	if (argc > 1)
+	{
+		m = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", m);
+	}
+	printf("Error\n");
+	return (0);
+}
