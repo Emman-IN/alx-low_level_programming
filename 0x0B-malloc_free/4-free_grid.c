0fac38680ecd3b7abb01a92f940f765c1a926564
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-10-05 06:53:29 -0700

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * free_grid - frees a 2 dimensional grid
+ * @grid: 2D grid
+ * @height: no of rows in grid
+ * Return: none
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i, j;
+
+	for (i = 0; i < height; i++)
+	{
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				free(grid[i];
+			}
+			free(grid);
+		}
+	}
+}
