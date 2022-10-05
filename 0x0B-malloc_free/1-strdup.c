2b87e848f7bb56d92d3fb90577ed4966e050619c
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-10-05 02:44:59 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -20,7 +20,7 @@ char *_strdup(char *str)
 	}
 	for (len = 0; str[len]; len++)
 		;
-	new_str = (char *)malloc(sizeof(char) * len);
+	new_str = (char *)malloc(sizeof(char) * len + 1);
 
 	if (new_str == NULL)
 	{
