c7057fbc75a1882ece29857bd9e2087c5ab0595d
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-10-05 01:38:37 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_array - creates array with one char
+ * @size: size of array
+ * @c: char to fill array with
+ * Return: array created
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	size_t i;
+	char *arr;
+
+	if (size == 0)
+		return (NULL);
+	arr = (char *)malloc(sizeof(char) * size);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	arr[size] = '\0';
+	return (arr);
+}
