145aa73dbbd4687fe02a7ea7c2516ec1d635e2a7
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-10-05 04:10:51 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,18 +9,23 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, len1, len2;
+	size_t i, len1, len2, size;
 	char *concat;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	for (len1 = 0; s1[len1]; len1++)
 		;
 	for (len2 = 0; s2[len2]; len2++)
 		;
-	concat = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	size = len1 + len2 + 1;
+	concat = (char *)malloc(sizeof(char) * size);
 	if (concat == NULL)
 	{
 		return (NULL);
@@ -29,9 +34,10 @@ char *str_concat(char *s1, char *s2)
 	{
 		concat[i] = s1[i];
 	}
-	for (i = 0; i < len2 && s2[i]; i++)
+	for (i = 0; s2[i]; i++)
 	{
 		concat[len1 + i] = s2[i];
 	}
+
 	return (concat);
 }
