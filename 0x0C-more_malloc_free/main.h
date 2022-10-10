507815917e12b1b6d01083cf2a353b83e9cdca8a
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-10-10 06:08:18 -0700

    1-init_dog.c

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN
+#define MAIN
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void *malloc_checked(unsigned int b);
+
+#endif
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef MAIN
+#define MAIN
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+typedef struct dog dog_t;
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+
