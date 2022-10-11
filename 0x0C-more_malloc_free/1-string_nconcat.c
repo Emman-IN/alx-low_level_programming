74c739d6ceb140105113c5af751efdf53efc7976
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-10-11 09:17:15 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,23 +8,30 @@
  * Return: pointer to the string
  */
 
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
 	size_t len1, len2, i, j;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
 	for (len1 = 0; s1[len1]; len1++)
 		;
 	for (len2 = 0; s2[len2]; len2++)
 		;
-	ptr = malloc(len1 + len2 + 1);
+
+	ptr = malloc(len1 + n + 1);
 	if (ptr == NULL)
 	{
 		return (NULL);
-	}
-	if (s1 == NULL || s2 == NULL)
-	{
-		return ("");
 	}
 	for (i = 0; s1[i]; i++)
 	{
