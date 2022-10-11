c3fd79812753477644952f6cc032b6b2b2b55c83
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-10-11 09:04:53 -0700

    1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include"main.h"
+
+/**
+ * string_nconcat - concatenates two string
+ * @s1: string 1
+ * @s2: string 2
+ * @n: number of bytes from s2 to s1
+ * Return: pointer to the string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	int len1, len2, i;
+
+	for (len1 = 0; s1[len1]; len1++)
+		;
+	for (len2 = 0; s2[len2]; len2++)
+		;
+	ptr = malloc(len1 + len2 + 1);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	if (s1 == NULL || s2 == NULL)
+	{
+		return ("");
+	}
+	for (i = 0; s1[i]; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	if (n >= len2)
+	{
+		for (j = 0; s2[j]; j++)
+		{
+			ptr[len1 + i] = s2[j];
+		}
+	}
+	else
+	{
+		for (i = 0; i < n && s2[i]; i++)
+		{
+			ptr[len1 + i] = s2[i];
+		}
+	}
+	return(ptr);
+}
