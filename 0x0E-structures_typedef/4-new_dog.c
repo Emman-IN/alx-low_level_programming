2009cf0ede164b7f83b2a454dbd3e19e71808176
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-10-11 01:47:51 -0700

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,55 @@
+#include"main.h"
+
+/**
+ * new_dog - function that creates a new dog.
+ * creates a new structure variable of type dog_t
+ * initialise the struct members
+ *
+ * @name: name of struct member
+ * @age: age of struct member
+ * @owner: owner
+ * Return: pointer to struct variable of type dog_t
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int len_name, len_owner, i;
+	dog_t *dog;
+
+	for (len_name = 0; name[len_name]; len_name++)
+		;
+	for (len_owner = 0; owner[len_owner]; len_owner++)
+		;
+	dog = (dog_t *)malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->name = (char *)malloc(len_name);
+	if (dog->name == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; name[i]; i++)
+	{
+		dog->name[i] = name[i];
+	}
+	dog->age = age;
+	/** always 4bytes since its an int*/
+	dog->owner = (char *)malloc(len_owner);
+	if (dog->owner == NULL)
+	{
+		free(dog->owner);
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; owner[i]; i++)
+	{
+		dog->owner[i] = owner[i];
+	}
+	return (dog);
+}
