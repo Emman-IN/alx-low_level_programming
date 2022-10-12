8c3c74571f7717b30ddc6b7dd41bdf75bd58dbf7
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-10-11 19:49:02 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,14 +36,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	}
 
-	for (i = 0; s1[i]; i++)
+	for (i = 0; i < len1 + n + 1; i++)
 	{
-		ptr[i] = s1[i];
-	}
-
-	for (i = 0; i < n; i++)
-	{
-		ptr[len1 + i] = s2[i];
+		if (i < len1)
+		{
+			ptr[i] = s1[i];
+		}
+		else
+		{
+			ptr[i] = s2[i - len1];
+		}
 	}
 	ptr[i] = '\0';
 	return (ptr);
