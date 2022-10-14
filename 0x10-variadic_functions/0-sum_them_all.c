2239fd9175c83b76331cfc29f11f79a814efba5f
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-10-14 06:18:35 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: the fixed arg
+ * Return: sum of all parameters
+ */
+
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int i, num, sum = 0;
+
+	va_list ptr;
+
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(ptr, int);
+		sum += num;
+	}
+	va_end(ptr);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/main.h
@@ -0,0 +1,8 @@
+#ifndef M
+#define M
+
+#include<stdio.h>
+#include<stdarg.h>
+int sum_them_all(const unsigned int n, ...);
+
+#endif
