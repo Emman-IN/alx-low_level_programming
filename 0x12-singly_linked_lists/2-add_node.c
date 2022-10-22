cdaec43f5a29e8fddff377560ef1a5c7349daa3e
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-10-22 15:24:10 -0700

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to current node
+ * @str: string to be passed to ftn
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newnode;
+	int i;
+
+	newnode = malloc(sizeof(list_t));
+
+	if (!newnode)
+	{
+		return (NULL);
+	}
+
+	strcpy(newnode->str, str);
+	while (str[i])
+	{
+		i++;
+	}
+	newnode->len = i;
+
+	newnode->next = *head;
+	*head = newnode;
+	return (newnode);
+}
