a28749893622c8d9048498a6c42cc511dd189d43
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-10-22 06:04:21 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,6 +11,7 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 	while (h)
+
 	{
 		if (h->str == NULL)
 		{
@@ -18,7 +19,7 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%d] %s\n",h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		count++;
 		h = h->next;
