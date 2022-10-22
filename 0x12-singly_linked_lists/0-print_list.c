4dc64c3b7db17ba197e63c8753b16ba55e1972e0
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-10-22 05:56:31 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to current node/list item
+ * Return: number of nodes
+ */
+
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("%s\n", "[0] (nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n",h->len, h->str);
+		}
+		count++;
+		h = h->next;
+
+	}
+	return (count);
+}
