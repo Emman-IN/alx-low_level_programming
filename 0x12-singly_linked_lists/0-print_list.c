74498cfa0c2773cf6d70fb1c159ae84a6b75740e
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-10-22 20:18:00 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,11 +14,11 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-			printf("%s\n", "[0] (nil)");
+			printf( "[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		}
 		count++;
 		h = h->next;
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_node_end - add a node the beginning of a linked list
+ * @head: pointer reference to the first node of the linked list
+ * @str: pointer to the str to be added to the node
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+
+	int i;
+	list_t *current;
+
+	while (str[i])
+		i++;
+	if (!(*head))
+	{
+		(*head) = malloc(sizeof(list_t));
+		current = *head;
+	}
+	else
+	{
+		current = *head;
+		while (current->next)
+		{
+			current = current->next;
+		}
+		current->next = malloc(sizeof(list_t));
+		current = current->next;
+	}
+	current->str = strdup(str);
+	current->len = i;
+	current->next = NULL;
+	return (*head);
+}
