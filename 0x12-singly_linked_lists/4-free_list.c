ab962c213b5a833ddb742f080759cb627f1e0c32
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-10-23 06:16:33 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,16 +5,15 @@
  * @head: pointer to the linked list
  */
 
+
 void free_list(list_t *head)
 {
 	list_t *current = head;
 
 	while (head)
 	{
-		free(current->str);
 		current = current->next;
 		free(head);
-
-		head = current->next;
+		head = current;
 	}
 }
