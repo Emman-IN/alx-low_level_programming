62c6029ae7c2fa2fa07f081f4fb07c7d2320c0f2
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-10-23 08:01:19 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,18 +6,25 @@
  * Return: number of elements in the list.
  */
 
+
+
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t c = 0;
+	const list_t *current = h;
 
-	while (h)
+	while (current)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)");
+		if (!current->str)
+		{
+			printf("[0] (nil)\n");
+		}
 		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		count++;
+		{
+			printf("[%u] %s\n", current->len, current->str);
+		}
+		current = current->next;
+		c++;
 	}
-	return (count);
+	return (c);
 }
