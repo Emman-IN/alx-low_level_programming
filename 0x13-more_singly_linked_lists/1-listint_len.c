a5bb8865de2c69c5d2a1b57da6bbd76e04637c49
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-10-25 08:45:24 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,11 +9,12 @@
 size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
+	const listint_t *current = h;
 
 	while (h != NULL)
 	{
 		count++;
-		h = h->next;
+		current = current->next;
 	}
 
 	return (count);
