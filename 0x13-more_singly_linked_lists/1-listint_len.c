f6c786753de61920afcb3e0f508ea720b5d55398
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-10-25 07:40:17 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked list
+ * @h - pointer to head or first node
+ * Return: number of nodes or elements in a list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *current  = h;
+	size_t count = 0;
+
+	while (current)
+	{
+		count++;
+		current = current->next;
+	}
+	return (count);
+}
+
