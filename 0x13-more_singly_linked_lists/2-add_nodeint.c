6dcaeaa8cecc584c22f72beddac65712a594a34f
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-10-25 08:10:29 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,6 +11,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newNode = malloc(sizeof(listint_t));
 
+	if (!newNode)
+	{
+		return (NULL);
+	}
 	newNode->n = n;
 
 	listint_t *current = *head;
