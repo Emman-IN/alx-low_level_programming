9b8a73aacbc4e1bc0e63a18a6584527bd88e481c
Emmanuel Nwachukwu <nwaikechukwu50@gmail.com>
2022-10-30 08:28:57 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,38 +8,15 @@
 
 void print_binary(unsigned long int n)
 {
-	char *num = malloc(255);
-	int rem, i, len, j;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (n == 0)
-	{
-		putchar(48);
-	}
-
-	else if (n == 1)
+	if (n & 1)
 	{
 		putchar(49);
 	}
 	else
 	{
-		if (n > 1)
-		{
-			while (n / 2 != 0)
-			{
-				rem = n % 2;
-				num[i] = rem + '0';
-				i++;
-				n = n / 2;
-			}
-		}
-		num[i] = 1 + '0';
-		while (num[len])
-		{
-			len++;
-		}
-		for (j = len - 1; j >= 0; j--)
-		{
-			putchar(num[j]);
-		}
+		putchar(48);
 	}
 }
