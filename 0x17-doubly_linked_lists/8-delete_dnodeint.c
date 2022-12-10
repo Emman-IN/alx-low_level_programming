3f33418effb70da1ab08d7ab5bdd5e936cf72574
Emmanuel <nwaikechukwu50@gmail.com>
2022-12-10 03:49:12 -0800

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -19,6 +19,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (!*head)
 			return (-1);
 		*head = (*head)->next;
+		(*head)->prev = NULL;
 		free(current);
 		return (1);
 	}
