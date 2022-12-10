c961e0d1d3d874fdcb62da6dcf1fd1ab5fd8538a
Emmanuel <nwaikechukwu50@gmail.com>
2022-12-10 07:02:56 -0800

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,16 +6,16 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current = *head;
+	listint_t *current;
 
 	if (*head == NULL)
 		exit(1);
 
 	while (*head)
 	{
+		current = *head;
 		*head = (*head)->next;
 		free(current);
-		current = *head;
 	}
 
 }
