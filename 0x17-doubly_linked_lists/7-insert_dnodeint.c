a4084422e7c57bb128839bfd2806cdf139ae1439
Emmanuel <nwaikechukwu50@gmail.com>
2022-12-10 02:34:33 -0800

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -47,7 +47,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		newnode->next = current->next;
 		newnode->prev = current;
 		current->next = newnode;
-		current->next->prev = newnode;
+		newnode->next->prev = newnode;
 	}
 	return (newnode);
 }
